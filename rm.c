d54757c8d3b9748af610b0d8a08dc290b1212856
Emre Karatas <85746327+emre-karatas@users.noreply.github.com>
2023-05-06 23:57:23 +0300

    Update rm.c

--- rm.c
+++ rm.c
@@ -1,87 +1,62 @@
-#include <stdio.h>
-#include <pthread.h>
-#include <stdlib.h>
 #include "rm.h"
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdio.h>
 
+int num_threads;
+int num_resource_types;
+int existing_resources[MAXR];
+bool deadlock_avoidance;
 
-// global variables
+int rm_init(int p_count, int r_count, int r_exist[], int avoid) {
+    if (p_count <= 0 || p_count > MAXP || r_count <= 0 || r_count > MAXR || (avoid != 0 && avoid != 1)) {
+        return -1;
+    }
 
-int DA;  // indicates if deadlocks will be avoided or not
-int N;   // number of processes
-int M;   // number of resource types
-int ExistingRes[MAXR]; // Existing resources vector
+    num_threads = p_count;
+    num_resource_types = r_count;
+    for (int i = 0; i < r_count; i++) {
+        if (r_exist[i] < 0) {
+            return -1;
+        }
+        existing_resources[i] = r_exist[i];
+    }
 
-//..... other definitions/variables .....
-//.....
-//.....
+    deadlock_avoidance = avoid == 1;
 
-// end of global variables
-
-int rm_thread_started(int tid)
-{
-    int ret = 0;
-    return (ret);
+    return 0;
 }
 
-
-int rm_thread_ended()
-{
-    int ret = 0;
-    return (ret);
+int rm_thread_started(int tid) {
+    // Implement the function to handle a new thread starting
+    return 0;
 }
 
-
-int rm_claim (int claim[])
-{
-    int ret = 0;
-    return(ret);
+int rm_thread_ended() {
+    // Implement the function to handle a thread ending
+    return 0;
 }
 
-
-int rm_init(int p_count, int r_count, int r_exist[],  int avoid)
-{
-    int i;
-    int ret = 0;
-    
-    DA = avoid;
-    N = p_count;
-    M = r_count;
-    // initialize (create) resources
-    for (i = 0; i < M; ++i)
-        ExistingRes[i] = r_exist[i];
-    // resources initialized (created)
-    
-    //....
-    // ...
-    return  (ret);
+int rm_claim(int claim[]) {
+    // Implement the function to handle resource claim for deadlock avoidance
+    return 0;
 }
 
-
-int rm_request (int request[])
-{
-    int ret = 0;
-    
-    return(ret);
+int rm_request(int request[]) {
+    // Implement the function to handle resource request
+    return 0;
 }
 
-
-int rm_release (int release[])
-{
-    int ret = 0;
-
-    return (ret);
+int rm_release(int release[]) {
+    // Implement the function to handle resource release
+    return 0;
 }
 
-
-int rm_detection()
-{
-    int ret = 0;
-    
-    return (ret);
+int rm_detection() {
+    // Implement the function to detect deadlocks
+    return 0;
 }
 
-
-void rm_print_state (char hmsg[])
-{
-    return;
+void rm_print_state(char headermsg[]) {
+    // Implement the function to print the current state of the resource manager
 }
